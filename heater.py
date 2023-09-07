ee8bb4e4568aede8e8bc3de1b2f69ef7733a648e
eduardostern <92046555+eduardostern@users.noreply.github.com>
2023-09-06 21:58:47 -0300

    Update heater.py

--- heater.py
+++ heater.py
@@ -1,8 +1,14 @@
 # Main File
 # Changed
 
-import time, update
+import time, update, machine
+
+power = machine.Pin(2, machine.Pin.OUT)
 
 while 1:
-    time.sleep(10)
+    power.value(1)
+    time.sleep(5)
+    power.value(0)
+    time.sleep(5)
+
     update.update_file()
