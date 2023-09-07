a679dc6775fa413ca1891522d1bdda89f03ce596
eduardostern <92046555+eduardostern@users.noreply.github.com>
2023-09-06 22:04:20 -0300

    Update heater.py

--- heater.py
+++ heater.py
@@ -3,12 +3,41 @@
 
 import time, update, machine
 
-power = machine.Pin(2, machine.Pin.OUT)
+led = machine.Pin(2, machine.Pin.OUT)
+led = machine.Pin(0, machine.Pin.OUT)
 
 while 1:
-    power.value(1)
-    time.sleep(5)
+    led.value(1)
     power.value(0)
-    time.sleep(5)
+    time.sleep(1)
+    led.value(0)
+    time.sleep(1)
+
+    led.value(1)
+    time.sleep(1)
+    led.value(0)
+    time.sleep(1)
+
+    led.value(1)
+    time.sleep(1)
+    led.value(0)
+    time.sleep(1)
+
+    power.value(1)
+
+    led.value(1)
+    time.sleep(1)
+    led.value(0)
+    time.sleep(1)
+
+    led.value(1)
+    time.sleep(1)
+    led.value(0)
+    time.sleep(1)
+
+    led.value(1)
+    time.sleep(1)
+    led.value(0)
+    time.sleep(1)
 
     update.update_file()
