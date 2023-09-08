8d7f8dc41ec5c5b627d44eac0e2fce42f079f4f1
eduardostern <92046555+eduardostern@users.noreply.github.com>
2023-09-08 00:02:05 -0300

    Update heater.py

--- heater.py
+++ heater.py
@@ -1,28 +1,68 @@
 # upython-update-file eduardostern
 # Heater Controller uPython
 
-import time, update, machine, onewire, ds18x20, ubinascii
+import utime, update, machine, onewire, ds18x20, ubinascii, ntptime
 from umqtt.simple import MQTTClient
 
 
-
+pool_setpoint=30.0
+pool_histeresis=1.0
+heater_mode=0
+nextpublish = utime.time()
+nextled = utime.time()
 
 
 def sub_cb(topic, msg):
-    print((topic.decode(), msg.decode()))
+    
+    global pool_setpoint
+    global heater_mode
+    global nextpublish
+    global nextled
+
+    print(topic.decode(), msg.decode())
+    mqtt_topic = topic.decode()
+    mqtt_msg = msg.decode()
+    mqtt_cmd = mqtt_topic.split('/')[2]
+    if mqtt_cmd == 'Br':
+        print('Berry Emulation persist object')
+        br_cmd = mqtt_msg.split(';')[0]
+        persist=br_cmd
+        p_var = persist.split('=')[0]
+        p_value = persist.split('=')[1]
+        
+        print(p_var, p_value)
+        
+        if p_var == 'persist.heater_mode':
+            heater_mode=int(p_value)
+            nextled = utime.time()
+            nextpublish = utime.time()
+        
+        if p_var == 'persist.pool_setpoint':
+            pool_setpoint=float(p_value)
+            nextled = utime.time()
+            nextpublish = utime.time()
+            
+    
 
 def main():
+    ntptime.settime()
+
+  
+    global pool_setpoint
+    global heater_mode
+    global nextpublish
+    global pool_histeresis
+    global nextled
+    
 
     led = machine.Pin(2, machine.Pin.OUT)
     power = machine.Pin(4, machine.Pin.OUT)
-    nextupdate = time.time()+60
-    nextread = time.time()
-    nextled = time.time()
+    nextupdate = utime.time()+60
+    nextled = utime.time()
+    nextpublish = utime.time()
 
     running=0
-    pool_setpoint=30
-    heater_mode=0
-
+  
 
     ds_pin = machine.Pin(0)
     ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
@@ -46,26 +86,42 @@ def main():
         mqttClient.check_msg()
 
         
-        if time.time() >= nextupdate:
+        if utime.time() >= nextupdate:
             update.update_file()
-            nextupdate=time.time()+60
+            nextupdate=utime.time()+60
             
-        if time.time() >= nextread:
-            
-            current_time = time.time()
-            message = '{"utc":'+str(current_time)+', "mode":'+str(heater_mode)+', "current_temp":'+str(current_temp)+', "pool_setpoint":'+str(pool_setpoint)+', "running": 0}'
-            
-            mqttClient.publish('/tele/'+CLIENT_ID+'/RESULT', str(message).encode())
-            print(message)
-            
-            nextread=time.time()+30
-            
-        if time.time() >= nextled:
+        
+        if utime.time() >= nextled:
             led.value(0)
             ds_sensor.convert_temp()
             current_temp = ds_sensor.read_temp(roms[0])
             mqttClient.ping()
             led.value(1)
-            nextled=time.time()+1
+            
+            if current_temp < (pool_setpoint-pool_histeresis):
+                running=1
+            if current_temp >= pool_setpoint:
+                running=0
+            
+            if heater_mode==0:
+                running=0
+                
+            power.value(running)
+            
+            nextled=utime.time()+1
+        
+        
+        if utime.time() >= nextpublish:
+            
+            current_time = 946684800 + utime.time()
+            message = '{"utc":'+str(current_time)+', "mode":'+str(heater_mode)+', "current_temp":'+str(current_temp)+', "pool_setpoint":'+str(pool_setpoint)+', "running": '+str(running)+'}'
+            topic='tele/'+CLIENT_ID+'/RESULT'
+            
+            mqttClient.publish(topic, str(message).encode())
+            print(message, topic)
+            
+            nextpublish=utime.time()+30
+            
+        
 
     
